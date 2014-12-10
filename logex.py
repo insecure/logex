b1885b9faede3eab151f4c55572b7893d330d5cd
Tobias Hommel <github@genoetigt.de>
2014-12-10 23:34:34 +0100

    log exceptions that occur while creating log message detect wrapped logex calls and abbreviate source view cleanup traceback object to prevent cyclic references more examples

--- logex.py
+++ logex.py
@@ -58,8 +58,20 @@ LAZY = False
 RERAISE = True
 CATCHALL = False
 VIEW_SOURCE = False
+DETECT_WRAPPED = True
 
-def _generate_source_view(tb):
+_logger = logging.getLogger('logex')
+
+
+def _get_next_code_name(tb, wrapper_code):
+	while tb is not None:
+		if tb.tb_frame.f_code is wrapper_code:
+			tb = tb.tb_next
+		else:
+			return tb.tb_frame.f_code.co_name
+	return '<unknown>'
+
+def _generate_source_view(tb, wrapper_code):
 	"""Generate a view showing the source code for all frames in a given traceback.
 	The view contains the following information for every frame in the traceback:
 	 * the name of the current function
@@ -71,11 +83,21 @@ def _generate_source_view(tb):
 
 	:param tb: a traceback object (e.g. from sys.exc_info()[2])
 	:type tb: types.TracebackType
+	:param wrapper_code: the code object of the wrapper function, used to detect nested logex calls if not None
+	:param wrapper_code: types.CodeType or None
 	:rtype: str
 	"""
 	source_view = ['========== sourcecode ==========']
 	while tb is not None:
 		crashed_frame = tb.tb_frame
+		if wrapper_code is not None:
+			if tb.tb_frame.f_code is wrapper_code:
+				next_code_name = _get_next_code_name(tb, wrapper_code)
+				source_view.extend(['-------------------------------------------------------',
+									'-- detected nested logex calls, see previous message --',
+									'-- for call to %-37.37s --' % (next_code_name+'()'),
+									'-------------------------------------------------------'])
+				break
 		crashed_line = crashed_frame.f_lineno
 		try:
 			filename = inspect.getsourcefile(crashed_frame)
@@ -144,7 +166,7 @@ def _is_method_of(func_name, class_object):
 	else:
 		return inspect.ismethod(attr)
 
-def generate_log_message(template, args, kwargs, exc, view_source=None):
+def generate_log_message(template, args, kwargs, exc, wrapper_code=None, view_source=None):
 	"""Generate a message based on a given template.
 
 	:param template: a template for the returned message, the following place holders will be replaced:
@@ -160,6 +182,8 @@ def generate_log_message(template, args, kwargs, exc, view_source=None):
 	:param kwargs: the keyword arguments to the top function of the traceback
 	:type kwargs: dict
 	:param exc: a (type, value, traceback) tuple as returned by sys.exc_info()
+	:param wrapper_code: the code object of the wrapper function, used to detect nested logex calls if not None
+	:param wrapper_code: types.CodeType or None
 	:param view_source: if True, add a view for the source code of every relevant function in the exception traceback
 	:type view_source: bool
 	:rtype: str
@@ -170,7 +194,7 @@ def generate_log_message(template, args, kwargs, exc, view_source=None):
 	func_name = tb_.tb_frame.f_code.co_name
 	if view_source:
 		try:
-			source = _generate_source_view(tb_)
+			source = _generate_source_view(tb_, wrapper_code=wrapper_code)
 		except Exception:
 			source = 'Error generating source view:\n%s' % traceback.format_exc()
 	else:
@@ -189,7 +213,8 @@ def generate_log_message(template, args, kwargs, exc, view_source=None):
 		'sourceview': source
 	}
 
-def _handle_log_exception(args, kwargs, logfunction, lazy, advanced, template, view_source, reraise, strip=1):
+def _handle_log_exception(args, kwargs, logfunction, lazy, advanced,
+						  template, view_source, reraise, wrapper_code=None, strip=1):
 	# noinspection PyBroadException
 	try:
 		logf = logfunction() if lazy else logfunction
@@ -199,21 +224,28 @@ def _handle_log_exception(args, kwargs, logfunction, lazy, advanced, template, v
 				break
 			tb_ = tb_.tb_next
 		if advanced:
-			logf(template, args, kwargs, (type_, value_, tb_),
+			logf(template, args, kwargs, (type_, value_, tb_), wrapper_code=wrapper_code,
 				 view_source=view_source)
 		else:
-			logf(generate_log_message(
-				template, args, kwargs, (type_, value_, tb_),
-				view_source=view_source))
+			message = generate_log_message(
+				template, args, kwargs, (type_, value_, tb_), wrapper_code=wrapper_code,
+				view_source=view_source)
+			logf(message)
 	except Exception:
-		traceback.print_exc()
-		pass #TODO: any sane way to report this? logging/print is not generic enough
+		logging.basicConfig()
+		_logger.exception('Error while generating log message for unhandled exception:')
+	finally:
+		try:
+				# noinspection PyUnboundLocalVariable
+				del tb_
+		except NameError:
+			pass
 	if reraise:
 		# noinspection PyCompatibility
 		raise
 
 def log(wrapped_f=None, logfunction=None, lazy=None, advanced=None, template=None,
-		reraise=None, catchall=None, view_source=None):
+		reraise=None, catchall=None, view_source=None, detect_wrapped=None):
 	"""Decorator function that logs all unhandled exceptions in a function.
 	This is especially useful for thread functions in a daemon or functions which are used as D-Bus signal handlers.
 
@@ -237,6 +269,9 @@ def log(wrapped_f=None, logfunction=None, lazy=None, advanced=None, template=Non
 	:type catchall: bool
 	:param view_source: if True, include the source code of the failed function if possible
 	:type view_source: bool
+	:param detect_wrapped: if False, do not try to detect wrapped logex calls, i.e. if a method decorated by this
+	function calls another method decorated by this function
+	:type detect_wrapped: bool
 	"""
 	if logfunction is None: logfunction = LOGFUNCTION
 	if lazy is None: lazy = LAZY
@@ -245,6 +280,7 @@ def log(wrapped_f=None, logfunction=None, lazy=None, advanced=None, template=Non
 	if reraise is None: reraise = RERAISE
 	if catchall is None: catchall = CATCHALL
 	if view_source is None: view_source = VIEW_SOURCE
+	if detect_wrapped is None: detect_wrapped = DETECT_WRAPPED
 	if wrapped_f is not None:
 		if catchall:
 			# noinspection PyBroadException,PyDocstring
@@ -252,14 +288,24 @@ def log(wrapped_f=None, logfunction=None, lazy=None, advanced=None, template=Non
 				try:
 					wrapped_f(*args, **kwargs)
 				except:
-					_handle_log_exception(args, kwargs, logfunction, lazy, advanced, template, view_source, reraise)
+					if detect_wrapped:
+						wrapper_code = wrapper_f.func_code
+					else:
+						wrapper_code = None
+					_handle_log_exception(args, kwargs, logfunction, lazy, advanced,
+										  template, view_source, reraise, wrapper_code=wrapper_code)
 		else:
 			# noinspection PyBroadException,PyDocstring
 			def wrapper_f(*args, **kwargs):
 				try:
 					wrapped_f(*args, **kwargs)
 				except Exception:
-					_handle_log_exception(args, kwargs, logfunction, lazy, advanced, template, view_source, reraise)
+					if detect_wrapped:
+						wrapper_code = wrapper_f.func_code
+					else:
+						wrapper_code = None
+					_handle_log_exception(args, kwargs, logfunction, lazy, advanced,
+										  template, view_source, reraise, wrapper_code=wrapper_code)
 		return functools.update_wrapper(wrapper_f, wrapped_f)
 	else:
 		# noinspection PyDocstring
@@ -267,5 +313,5 @@ def log(wrapped_f=None, logfunction=None, lazy=None, advanced=None, template=Non
 			return log(wrapped_fn,
 					   logfunction=logfunction, lazy=lazy, advanced=advanced,
 					   template=template, reraise=reraise, catchall=catchall,
-					   view_source=view_source)
+					   view_source=view_source, detect_wrapped=detect_wrapped)
 		return arg_wrapper
